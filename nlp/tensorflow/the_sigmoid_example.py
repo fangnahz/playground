d4c21203c688c6c0e8eb18e4f0c9a81248006ac3
Zhang, Fan <fanz@aidigger.com>
2018-08-26 07:45:20 +0800

    update

--- nlp/tensorflow/the_sigmoid_example.py
+++ nlp/tensorflow/the_sigmoid_example.py
@@ -15,27 +15,37 @@ def the_sigmoid_example(W, b, session):
     #   4.1. 准备各种材料（常数，变量）
     #   4.2. 保存制作过程中的半成品材料（中间变量）
     # placeholder 是一个输入占位符，输入，用于算法的训练、测试
+    # x 不是变量，不需要定义后执行 global_variables_initializer
     x = tf.placeholder(
         dtype=tf.float32,  # 占位符要求的输入数据类型
         shape=[1, 10],  # 占位符形状，一维向量
         name='x'  # 占位符名字，可选，在调试过程中使用
                   # tensorflow 不知道 python 框架中使用的变量名（上面的 x），只知道这里指定的名称
-    )  # x 不是变量，不需要定义后执行 global_variables_initializer
+    )
     # 5. 主厨（operation executor）使用帮厨提供的材料根据顾客订单制作食物
     # operation: 使用输入、算法计算输出
     h = tf.nn.sigmoid(tf.matmul(x, W) + b)
     # 6. 总厨拿到主厨做好交出的食物，交给服务员，服务员把食物送给顾客
     # 执行运算获得输出，immutable tensors，保存最终结果，以及中间结果
-    h_eval = session.run(h, feed_dict={x: np.random.rand(1, 10)})
+    h_eval = session.run(
+        h,
+        feed_dict={x: np.random.rand(1, 10)}  # feed_dict 参数给 placeholder 赋值
+    )
     print('sigmoid: %s' % h_eval)
 
 
 def sigmoid_preloaded(W, b, session):
     # x: pre-loaded input
     # x 不是变量，不需要定义后执行 global_variables_initializer
-    x = tf.constant(value=[[.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.]], dtype=tf.float32, name='x')
+    # 使用 constant 而不是 placeholder，不需要在执行 session.run 的时候赋值
+    # 缺点：x 固定，不能在执行 session.run 时使用不同的 x 观察输出变化
+    x = tf.constant(
+        value=[[.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.]],
+        dtype=tf.float32,
+        name='x'
+    )
     h = tf.nn.sigmoid(tf.matmul(x, W) + b)
-    h_eval = session.run(h)
+    h_eval = session.run(h)  # 不需要提供 feed_dict 参数
     print('preloaded sigmoid: %s' % h_eval)
 
 if __name__ == '__main__':
