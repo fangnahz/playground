1fdf6b7c3accc5e2bd5faec82236cda9a2e113d6
Zhang, Fan <fanz@aidigger.com>
2018-08-26 07:32:54 +0800

    update

--- nlp/tensorflow/the_sigmoid_example.py
+++ nlp/tensorflow/the_sigmoid_example.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 
 
-def the_sigmoid_example():
+def the_sigmoid_example(W, b, session):
     '''
     The sigmoid example:
       h = sigmoid(W * x + b)
@@ -20,21 +20,23 @@ def the_sigmoid_example():
         shape=[1, 10],  # 占位符形状，一维向量
         name='x'  # 占位符名字，可选，在调试过程中使用
                   # tensorflow 不知道 python 框架中使用的变量名（上面的 x），只知道这里指定的名称
-    )
-    # variables: mutalbe tensors，算法中使用的变量
-    W = tf.Variable(tf.random_uniform(
-        shape=[10, 5], minval=-0.1, maxval=0.1, dtype=tf.float32), name='W'
-    )
-    b = tf.Variable(tf.zeros(shape=[5], dtype=tf.float32), name='b')
+    )  # x 不是变量，不需要定义后执行 global_variables_initializer
     # 5. 主厨（operation executor）使用帮厨提供的材料根据顾客订单制作食物
     # operation: 使用输入、算法计算输出
     h = tf.nn.sigmoid(tf.matmul(x, W) + b)
-    tf.global_variables_initializer().run()
     # 6. 总厨拿到主厨做好交出的食物，交给服务员，服务员把食物送给顾客
     # 执行运算获得输出，immutable tensors，保存最终结果，以及中间结果
     h_eval = session.run(h, feed_dict={x: np.random.rand(1, 10)})
-    print('h_eval: %s' % h_eval)
+    print('sigmoid: %s' % h_eval)
 
+
+def sigmoid_preloaded(W, b, session):
+    # x: pre-loaded input
+    # x 不是变量，不需要定义后执行 global_variables_initializer
+    x = tf.constant(value=[[.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.]], dtype=tf.float32, name='x')
+    h = tf.nn.sigmoid(tf.matmul(x, W) + b)
+    h_eval = session.run(h)
+    print('preloaded sigmoid: %s' % h_eval)
 
 if __name__ == '__main__':
     # 2. 服务员（session）会把客户点的餐记录在他的记事本，一个 tf.GraphDef 中
@@ -42,5 +44,15 @@ if __name__ == '__main__':
     # 3. session: 服务员
     #   3.1. 把客户点餐细节送到厨房，交给总厨（分布式 Master），总厨会分配任务给各个主厨、帮厨
     session = tf.InteractiveSession(graph=graph)
-    the_sigmoid_example()
+
+    # variables: mutalbe tensors，算法中使用的变量
+    W = tf.Variable(tf.random_uniform(
+        shape=[10, 5], minval=-0.1, maxval=0.1, dtype=tf.float32), name='W'
+    )
+    b = tf.Variable(tf.zeros(shape=[5], dtype=tf.float32), name='b')
+    tf.global_variables_initializer().run()  # 初始化变量
+
+    the_sigmoid_example(W, b, session)
+    sigmoid_preloaded(W, b, session)
+
     session.close()
