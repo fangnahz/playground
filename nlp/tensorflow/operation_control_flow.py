1ad47db880c3020c3da6e64d978d6707475a0810
Zhang, Fan <fanz@aidigger.com>
2018-08-28 08:05:37 +0800

    udpate

---
+++ nlp/tensorflow/operation_control_flow.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+
+def wrong_order(x, session):
+    # tensorflow does not care about the statements order,
+    # however the code is written,
+    # unless explicitly told
+    x_assign_op = tf.assign(x, x+5)
+    z = x * 2
+    return session.run(z)
+
+
+def control_flow(x, session):
+    x_assign_op = tf.assign(x, x+5)
+    with tf.control_dependencies([x_assign_op]):
+        z = x * 2
+    return session.run(z)
+
+if __name__ == '__main__':
+    session = tf.InteractiveSession()
+    x = tf.Variable(tf.constant(2.0), name='x')
+    tf.global_variables_initializer().run()
+    wrong_answer = wrong_order(x, session)
+    print('\nexpects 14, got', wrong_answer)
+    right_anser = control_flow(x, session)
+    print('using tf.control_dependencies to control the operation flow, gives the right answer:\n', right_anser)
+    session.close()
