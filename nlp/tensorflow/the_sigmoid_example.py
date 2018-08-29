9c7d54b8b9446c4cbe04e6e2aade840d6a68ad61
Zhang, Fan <fanz@aidigger.com>
2018-08-30 07:46:17 +0800

    update

--- nlp/tensorflow/the_sigmoid_example.py
+++ nlp/tensorflow/the_sigmoid_example.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 
 
-def the_sigmoid_example(W, b, session):
+def sigmoid_placeholder(W, b, session):
     '''
     The sigmoid example:
       h = sigmoid(W * x + b)
@@ -14,11 +14,13 @@ def the_sigmoid_example(W, b, session):
     # 4. 主厨查看订单，告诉帮厨（parameter server
     #   4.1. 准备各种材料（常数，变量）
     #   4.2. 保存制作过程中的半成品材料（中间变量）
+
+    # 创建 graph
     # placeholder 是一个输入占位符，输入，用于算法的训练、测试
     # x 不是变量，不需要定义后执行 global_variables_initializer
     x = tf.placeholder(
-        dtype=tf.float32,  # 占位符要求的输入数据类型
         shape=[1, 10],  # 占位符形状，一维向量
+        dtype=tf.float32,  # 占位符要求的输入数据类型
         name='x'  # 占位符名字，可选，在调试过程中使用
                   # tensorflow 不知道 python 框架中使用的变量名（上面的 x），只知道这里指定的名称
     )
@@ -29,11 +31,13 @@ def the_sigmoid_example(W, b, session):
 
     # 6. 总厨拿到主厨做好交出的食物，交给服务员，服务员把食物送给顾客
     # 执行运算获得输出，immutable tensors，保存最终结果，以及中间结果
+    input = np.random.rand(1, 10)
+    print('using placeholder x:', input)
     h_eval = session.run(
         h,
-        feed_dict={x: np.random.rand(1, 10)}  # feed_dict 参数给 placeholder 赋值
+        feed_dict={x: input}  # feed_dict 参数给 placeholder x 赋值
     )
-    print('\nsigmoid: %s\n' % h_eval)
+    print('result: %s\n' % h_eval)
 
 
 def sigmoid_preloaded(W, b, session):
@@ -46,17 +50,19 @@ def sigmoid_preloaded(W, b, session):
         dtype=tf.float32,
         name='x'
     )
+    print('preload using constant x:', x.eval().tolist())
 
     h = tf.nn.sigmoid(tf.matmul(x, W) + b)
 
     h_eval = session.run(h)  # 不需要提供 feed_dict 参数
-    print('preloaded sigmoid: %s\n' % h_eval)
+    print('result: %s\n' % h_eval)
 
 
 def sigmoid_pipeline(W, b, session):
     # input pipelines 用于快速处理大量数据
     # 使用 queue 传递数据，支持预处理，支持并发
     # 创建输入管道
+    print('using pipeline:')
     filenames = ['test%d.txt' % i for i in range(1, 4)]
     filename_queue = tf.train.string_input_producer(
         filenames,
@@ -95,22 +101,22 @@ def sigmoid_pipeline(W, b, session):
         min_after_dequeue=1,
         num_threads=1
     )
-    # Coordinator 类可以视为 thread manager，比如可以使用多线程 enqueue，dequeue 等，
+    # Coordinator 类可以视为 thread manager，管理使用多线程 enqueue，dequeue 等，
     # 管理多个 QueueRunners
     coord = tf.train.Coordinator()
-    # 自动创建 QueueRunner，从定义好的 queue 中获取数据，需要 explicitly 执行
+    # 自动创建 QueueRunners，从定义好的 queue (filename_queue) 中获取数据，需要 explicitly 执行
     threads = tf.train.start_queue_runners(coord=coord, sess=session)
 
     h = tf.nn.sigmoid(tf.matmul(x, W) + b)
 
     # 使用上面得到的 x 计算 h，打印出（5 步）执行的结果
     for step in range(5):
-        x_eval, h_evla = session.run([x, h])  # 如果不指定 x，则 parameter server 不保存 x 结果，运算一样可以正常执行
-        print('========== Setp %d ==========' % step)
+        x_eval, h_eval = session.run([x, h])  # 如果不指定 x，则 parameter server 不保存 x 结果，运算一样可以正常执行
+        print('========== Step %d ==========' % step)
         print('Evaluated data (x)')
         print(x_eval)
         print('Evaluated data (h)')
-        print('%s\n' % h_evla)
+        print('%s' % h_eval)
 
     # 最后需要停止、join threads，否则会一直 hang 住
     coord.request_stop()
@@ -123,7 +129,9 @@ if __name__ == '__main__':
     #   3.1. 把客户点餐细节送到厨房，交给总厨（分布式 Master），总厨会分配任务给各个主厨、帮厨
     session = tf.InteractiveSession(graph=graph)
 
-    # variables: mutalbe tensors，算法中使用的变量
+    print('\nCalculate sigmoid: h = sigmoid(x * W + b)')
+
+    # variables: mutalbe tensors，算法中使用的变量，定义后需要执行 global_variables_initializer
     W = tf.Variable(
         tf.random_uniform(
             shape=[10, 5],  # 每个纬度的 size，这个变量的长度是 tensor 的 rank，注意与矩阵 rank 概念的区别
@@ -145,8 +153,10 @@ if __name__ == '__main__':
     # 初始化变量，在这个命令后如果再定义变量，就需要定义以后再次执行，否则变量在 operation 中不可用，会抛出异常:
     #   FailedPreconditionError (see above for traceback): Attempting to use uninitialized value <var_name>
     tf.global_variables_initializer().run()
+    print('W:\n%s' % W.eval())  # 变量初始化以后才可以使用
+    print('b:%s\n' % b.eval().tolist())  # 变量初始化以后才可以使用
 
-    the_sigmoid_example(W, b, session)
+    sigmoid_placeholder(W, b, session)
     sigmoid_preloaded(W, b, session)
     sigmoid_pipeline(W, b, session)
 
