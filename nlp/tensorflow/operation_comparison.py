4120b25c830e5d4a4afa091977a87654ce913307
Zhang, Fan <fanz@aidigger.com>
2018-08-30 08:00:38 +0800

    update

--- nlp/tensorflow/operation_comparison.py
+++ nlp/tensorflow/operation_comparison.py
@@ -1,34 +1,16 @@
 import tensorflow as tf
 
 
-def equal_to(x, y, session):
-    x_equal_y = tf.equal(x, y, name=None)
-    result = session.run(x_equal_y)
-    return result
-
-
-def less_than(x, y, session):
-    x_less_y = tf.less(x, y)
-    result = session.run(x_less_y)
-    return result
-
-
-def greater_equal(x, y, session):
-    x_great_equal_y = tf.greater_equal(x, y)
-    result = session.run(x_great_equal_y)
-    return result
-
-
-def condition_select(x, y, session):
+def condition_select(x, y):
     condition = tf.constant([[True, False], [True, False]], dtype=tf.bool)
-    print('# select element from x if condition is True, else from y\n# condition:\n%s' % condition.eval())
+    print('# select element from x if condition is True, else from y\ncondition:\n%s' % condition.eval())
     x_cond_y = tf.where(condition, x, y)
-    result = session.run(x_cond_y)
-    return result
+    return x_cond_y
 
 if __name__ == '__main__':
     graph = tf.Graph()
     session = tf.InteractiveSession(graph=graph)
+    print('\nComparisions:')
     x = tf.constant(
         [
             [1, 2],
@@ -36,7 +18,7 @@ if __name__ == '__main__':
         ],
         dtype=tf.int32
     )
-    print('\nx:\n%s' % x.eval())
+    print('x:\n%s' % x.eval())
     y = tf.constant(
         [
             [4, 3],
@@ -45,8 +27,16 @@ if __name__ == '__main__':
         dtype=tf.int32
     )
     print('y:\n%s\n' % y.eval())
-    print('x = y:\n%s\n' % equal_to(x, y, session))
-    print('x < y:\n%s\n' % less_than(x, y, session))
-    print('x >= y:\n%s\n' % greater_equal(x, y, session))
-    print('select(x, y, condition):\n%s\n' % condition_select(x, y, session))
+
+    x_equal_y = tf.equal(x, y, name=None)
+    x_less_y = tf.less(x, y)
+    x_great_equal_y = tf.greater_equal(x, y)
+
+    print('x = y:\n%s\n' % session.run(x_equal_y))
+    print('x < y:\n%s\n' % session.run(x_less_y))
+    print('x >= y:\n%s\n' % session.run(x_great_equal_y))
+
+    x_cond_y = condition_select(x, y)
+    print('x condition y:\n%s\n' % session.run(x_cond_y))
+
     session.close()
