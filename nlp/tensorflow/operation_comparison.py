997b17507e0c7bf31faf26ac6204870e27554d16
Zhang, Fan <fanz@aidigger.com>
2018-08-30 08:09:41 +0800

    update

--- nlp/tensorflow/operation_comparison.py
+++ nlp/tensorflow/operation_comparison.py
@@ -3,14 +3,14 @@ import tensorflow as tf
 
 def condition_select(x, y):
     condition = tf.constant([[True, False], [True, False]], dtype=tf.bool)
-    print('# select element from x if condition is True, else from y\ncondition:\n%s' % condition.eval())
+    print('Condition:\n%s' % condition.eval())
     x_cond_y = tf.where(condition, x, y)
     return x_cond_y
 
 if __name__ == '__main__':
     graph = tf.Graph()
     session = tf.InteractiveSession(graph=graph)
-    print('\nComparisions:')
+    print('\nCompare X and Y:')
     x = tf.constant(
         [
             [1, 2],
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         ],
         dtype=tf.int32
     )
-    print('x:\n%s' % x.eval())
+    print('X:\n%s' % x.eval())
     y = tf.constant(
         [
             [4, 3],
@@ -26,17 +26,18 @@ if __name__ == '__main__':
         ],
         dtype=tf.int32
     )
-    print('y:\n%s\n' % y.eval())
+    print('Y:\n%s\n' % y.eval())
 
     x_equal_y = tf.equal(x, y, name=None)
     x_less_y = tf.less(x, y)
     x_great_equal_y = tf.greater_equal(x, y)
 
-    print('x = y:\n%s\n' % session.run(x_equal_y))
-    print('x < y:\n%s\n' % session.run(x_less_y))
-    print('x >= y:\n%s\n' % session.run(x_great_equal_y))
+    print('Is X == Y (element-wise)?\n%s\n' % session.run(x_equal_y))
+    print('Is X < Y (element-wise)?\n%s\n' % session.run(x_less_y))
+    print('Is X >= Y (element-wise)?\n%s\n' % session.run(x_great_equal_y))
 
+    print('Select elements of X or Y depending on the condition')
     x_cond_y = condition_select(x, y)
-    print('x condition y:\n%s\n' % session.run(x_cond_y))
+    print('Result:\n%s' % session.run(x_cond_y))
 
     session.close()
