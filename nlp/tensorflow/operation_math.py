5172a2a1edab21b8e893a714e85a7a488f14028e
Zhang, Fan <fanz@aidigger.com>
2018-08-30 10:11:03 +0800

    update

--- nlp/tensorflow/operation_math.py
+++ nlp/tensorflow/operation_math.py
@@ -1,45 +1,14 @@
 import tensorflow as tf
 
 
-def tensor_add(x, y, session):
-    x_add_y = tf.add(x, y)
-    result = session.run(x_add_y)
-    return result
-
-
-def matrix_multiplication(x, y, session):
-    x_mul_y = tf.matmul(x, y)
-    result = session.run(x_mul_y)
-    return result
-
-
-def logarithm(x, session):
-    log_x = tf.log(x)
-    result = session.run(log_x)
-    return result
-
-
-def row_sum(x, session):
-    x_sum_1 = tf.reduce_sum(x, axis=[1], keepdims=False)
-    result = session.run(x_sum_1)
-    return result
-
-
-def column_sum(x, session):
-    x_sum_2 = tf.reduce_sum(x, axis=[0], keepdims=True)
-    result = session.run(x_sum_2)
-    return result
-
-
-def segment_sum(session):
+def segment_sum():
     data = tf.constant([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype=tf.float32)
-    print('data (casted to int): %s' % tf.cast(data, tf.int32).eval().tolist())
+    print('Vector:               %s' % tf.cast(data, tf.int32).eval().tolist())
     segments = [0, 0, 0, 1, 1, 2, 2, 2, 2, 2]
     segment_ids = tf.constant(segments, dtype=tf.int32)
-    print('three segments 0/1/2: %s' % segments)
-    x_seg_sum = tf.segment_sum(data, segment_ids)
-    result = session.run(x_seg_sum)
-    return result
+    print('Three segments 0/1/2: %s' % segments)
+    seg_sum = tf.segment_sum(data, segment_ids)
+    return seg_sum
 
 if __name__ == '__main__':
     graph = tf.Graph()
@@ -51,7 +20,7 @@ if __name__ == '__main__':
         ],
         dtype=tf.float32
     )
-    print('\nx:\n%s' % x.eval())
+    print('\nX:\n%s' % x.eval())
     y = tf.constant(
         [
             [4, 3],
@@ -59,11 +28,20 @@ if __name__ == '__main__':
         ],
         dtype=tf.float32
     )
-    print('y:\n%s\n' % y.eval())
-    print('x + y:\n%s\n' % tensor_add(x, y, session))
-    print('x * y (matrix mulitplication):\n%s\n' % matrix_multiplication(x, y, session))
-    print('log(x):\n%s\n' % logarithm(x, session))
-    print('x row sum:\n%s\n' % row_sum(x, session).tolist())
-    print('x column sum:\n%s\n' % column_sum(x, session).tolist())
-    print('segment sum:\n%s\n' % segment_sum(session).tolist())
+    print('Y:\n%s\n' % y.eval())
+
+    x_add_y = tf.add(x, y)
+    x_mul_y = tf.matmul(x, y)
+    log_x = tf.log(x)
+    x_sum_rows = tf.reduce_sum(x, axis=[1], keepdims=False)
+    x_sum_columns = tf.reduce_sum(x, axis=[0], keepdims=True)
+
+    print('X + Y:\n%s\n' % session.run(x_add_y))
+    print('XY (matrix mulitplication):\n%s\n' % session.run(x_mul_y))
+    print('log(X):\n%s\n' % session.run(log_x))
+    print('X sum rows:\n%s\n' % session.run(x_sum_rows))
+    print('X sum columns:\n%s\n' % session.run(x_sum_columns))
+    print('Segment sum of a vector')
+    seg_sum = segment_sum()
+    print('result:\n%s\n' % session.run(seg_sum))
     session.close()
