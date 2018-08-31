d76843757ddb992194f79ec6a9ef9068f498e331
Zhang, Fan <fanz@aidigger.com>
2018-08-31 10:42:55 +0800

    update

--- nlp/tensorflow/operation_control_flow.py
+++ nlp/tensorflow/operation_control_flow.py
@@ -1,27 +1,31 @@
 import tensorflow as tf
 
 
-def wrong_order(x, session):
+def wrong_order(x):
     # tensorflow does not care about the statements order,
     # however the code is written,
     # unless explicitly told
     x_assign_op = tf.assign(x, x+5)
     z = x * 2
-    return session.run(z)
+    return z
 
 
-def control_flow(x, session):
+def control_flow(x):
     x_assign_op = tf.assign(x, x+5)
     with tf.control_dependencies([x_assign_op]):
         z = x * 2
-    return session.run(z)
+    return z
 
 if __name__ == '__main__':
+    # 如果不传入则使用 default_graph
     session = tf.InteractiveSession()
+
     x = tf.Variable(tf.constant(2.0), name='x')
     tf.global_variables_initializer().run()
-    wrong_answer = wrong_order(x, session)
+    wrong_answer = session.run(wrong_order(x))
     print('\nexpects 14, gets\n', wrong_answer)
-    right_anser = control_flow(x, session)
+
+    right_anser = session.run(control_flow(x))
     print('\nusing tf.control_dependencies to control the operation flow, gives the right answer:\n', right_anser)
+
     session.close()
